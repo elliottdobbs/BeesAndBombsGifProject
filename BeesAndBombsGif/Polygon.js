230dd022232f806804cc5623eed1b3ee777a4289
elliottdobbs <elliottdobbs95@gmail.com>
2018-09-26 12:36:33 -0500

    Pushing For Website

---
+++ BeesAndBombsGif/Polygon.js
@@ -0,0 +1,77 @@
+
+function Line(xx1, yy1, xx2, yy2, a){
+    this.angle = a;
+    this.x1 = xx1;
+    this.y1 = yy1;
+    this.x2 = xx2;
+    this.y2 = yy2;
+}
+
+function Polygon(s, c, startPos){
+    
+    this.startPosition = startPos;
+    this.offset = shapeOffset * (s - 3);
+    this.color = c;
+    this.sides = s;
+    this.lines = [];
+    
+    this.dotx = width/2;
+    this.doty = this.startPosition.y + this.offset;
+    this.currentLine = 0;
+    this.speed = 2;
+    
+    var currentPos = createVector(0, 0);
+    var prevPos = createVector(this.startPosition.x, this.startPosition.y + this.offset);
+    var angle = 0;
+    var angleInc = -360/this.sides/180 * PI;
+    
+    for (var i = 0; i < this.sides; i++){
+        currentPos = createVector(prevPos.x + cos(angle)*sideLength,
+                                  prevPos.y + sin(angle)*sideLength);
+        this.lines[i] = new Line(prevPos.x, prevPos.y, currentPos.x, currentPos.y, angle);
+        
+        angle += angleInc;
+        prevPos = currentPos;
+    }
+    
+    this.draw = function(){
+        
+        stroke(this.color);
+        for (var i = 0; i < this.lines.length; i++){
+            line(this.lines[i].x1, this.lines[i].y1, this.lines[i].x2, this.lines[i].y2);
+        }
+    }
+    
+    this.drawCircle = function(){
+        fill(0);
+        stroke(0);
+        ellipse(this.dotx, this.doty, 6, 6);
+    }
+    
+    this.newDotPos = function(){
+        
+        let d = dist(this.dotx, this.doty, this.lines[this.currentLine].x2, this.lines[this.currentLine].y2);
+        
+        if (d < this.speed + 1){
+            let newTravelDist = this.speed - d;
+            this.dotx = this.lines[this.currentLine].x2;
+            this.doty = this.lines[this.currentLine].y2;
+
+            if (this.currentLine === this.sides - 1){
+                this.currentLine = 0;
+            }
+            else{
+                this.currentLine++;
+            }
+            
+            this.dotx += cos(this.lines[this.currentLine].angle) * newTravelDist;
+            this.doty += sin(this.lines[this.currentLine].angle) * newTravelDist;
+        }
+        else{
+            this.dotx += cos(this.lines[this.currentLine].angle) * this.speed;
+            this.doty += sin(this.lines[this.currentLine].angle) * this.speed;
+        }
+    }
+    
+}
+
---
+++ BeesAndBombsGif/sketch.js
@@ -0,0 +1,56 @@
+
+let sideLength = 100;
+let shapeOffset = sideLength/6.25;
+let speed = 2;
+
+var polys = [];
+
+function setup() {
+    createCanvas(600, 600);
+    
+    var startPos = createVector(width/2 - sideLength/2, height/2 + sideLength/2);
+    var index = 0;
+    for (var i = 3; i < 16; i++){
+        polys[index++] = new Polygon(i, color(cos((i-3)/7)*255, tan((i-3)/7)*255, sin((i-3)/7)*255), startPos);
+    }
+    
+    var speedMultipliers = [];
+    speedMultipliers[0] = 42;
+    speedMultipliers[1] = 52;
+    speedMultipliers[2] = 60;
+    speedMultipliers[3] = 66;
+    speedMultipliers[4] = 70;
+    speedMultipliers[5] = 72;
+    speedMultipliers[6] = 72;
+    speedMultipliers[7] = 70;
+    speedMultipliers[8] = 66;
+    speedMultipliers[9] = 60;
+    speedMultipliers[10] = 52;
+    speedMultipliers[11] = 42;
+    speedMultipliers[12] = 30;
+    
+    for (var i = 0; i < 13; i++){
+        speedMultipliers[i] /= 30;
+    }
+    
+    
+    for (var i = 0; i < 13; i++){
+        polys[i].speed = speedMultipliers[i] * speed;
+    }
+}
+
+function draw() {
+    background(200);
+
+    for (var i = 0; i < 13; i++){
+        polys[i].draw();
+    }
+    
+    for (var i = 0; i < 13; i++){
+        polys[i].drawCircle();
+    }
+    for (var i = 0; i < 13; i++){
+        polys[i].newDotPos();
+    }
+    
+}
