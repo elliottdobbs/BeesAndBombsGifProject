3a36ec12c1e67bc4add0ddf818ee75fef6db2b0c
Elliott Dobbs <elliottdobbs@Elliotts-MacBook-Pro.local>
2021-08-17 18:56:57 -0500

    Commented for Smoothstack Interview

--- BeesAndBombsGif/Polygon.js
+++ BeesAndBombsGif/Polygon.js
@@ -1,4 +1,5 @@
 
+//Line object to use with each polygon
 function Line(xx1, yy1, xx2, yy2, a){
     this.angle = a;
     this.x1 = xx1;
@@ -25,6 +26,7 @@ function Polygon(s, c, startPos){
     var angle = 0;
     var angleInc = -360/this.sides/180 * PI;
     
+    //Creating each vector for each side of the polygon based on the given side count.
     for (var i = 0; i < this.sides; i++){
         currentPos = createVector(prevPos.x + cos(angle)*sideLength,
                                   prevPos.y + sin(angle)*sideLength);
@@ -50,13 +52,22 @@ function Polygon(s, c, startPos){
     
     this.newDotPos = function(){
         
+        //Calculates the new distance the dot for this polygon needs to move
         let d = dist(this.dotx, this.doty, this.lines[this.currentLine].x2, this.lines[this.currentLine].y2);
         
+        //If the distance puts the dot past the current line it is travelling.  
+        //  The dot needs to move the remaining distance of the line 
+        //  then up the next line.
         if (d < this.speed + 1){
+
+            //Get the distance that the dot needs to travel up the new line.  
             let newTravelDist = this.speed - d;
+
+            //Sets the dots location to the end of the current line (same as the start to the new line)
             this.dotx = this.lines[this.currentLine].x2;
             this.doty = this.lines[this.currentLine].y2;
 
+            //If we are at the last line in the polygon
             if (this.currentLine === this.sides - 1){
                 this.currentLine = 0;
             }
@@ -64,10 +75,12 @@ function Polygon(s, c, startPos){
                 this.currentLine++;
             }
             
+            //Moves the dot up the line.
             this.dotx += cos(this.lines[this.currentLine].angle) * newTravelDist;
             this.doty += sin(this.lines[this.currentLine].angle) * newTravelDist;
         }
         else{
+            //Moves the dot up the line.
             this.dotx += cos(this.lines[this.currentLine].angle) * this.speed;
             this.doty += sin(this.lines[this.currentLine].angle) * this.speed;
         }
--- BeesAndBombsGif/sketch.js
+++ BeesAndBombsGif/sketch.js
@@ -1,19 +1,21 @@
 
+//Globals
 let sideLength = 100;
 let shapeOffset = sideLength/6.25;
 let speed = 2;
-
 var polys = [];
 
 function setup() {
     createCanvas(600, 600);
     
+    //Starting positions are simply evenly spaced just below the center of the graphic. Polygons are created.
     var startPos = createVector(width/2 - sideLength/2, height/2 + sideLength/2);
     var index = 0;
     for (var i = 3; i < 16; i++){
         polys[index++] = new Polygon(i, color(cos((i-3)/7)*255, tan((i-3)/7)*255, sin((i-3)/7)*255), startPos);
     }
     
+    //Speed controlling for the synchronization of the dot movement.
     var speedMultipliers = [];
     speedMultipliers[0] = 42;
     speedMultipliers[1] = 52;
@@ -33,7 +35,6 @@ function setup() {
         speedMultipliers[i] /= 30;
     }
     
-    
     for (var i = 0; i < 13; i++){
         polys[i].speed = speedMultipliers[i] * speed;
     }
@@ -42,6 +43,7 @@ function setup() {
 function draw() {
     background(200);
 
+    //Re-drawing each polygon with the new dot location.
     for (var i = 0; i < 13; i++){
         polys[i].draw();
         polys[i].drawCircle();
